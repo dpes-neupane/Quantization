a3386575d1658802aff85aeb636c6e2361988e53
dpes-neupane <dpesn47@gmail.com>
2025-09-11 18:46:00 +0545

    ssim class added

--- quantization/evaluation.py
+++ quantization/evaluation.py
@@ -1,7 +1,6 @@
 from torcheval.metrics import PeakSignalNoiseRatio
 from typing import List, Union
 from skimage.metrics import structural_similarity as ssim
-
 def calculatePSNR(preds, targets, device):
     metric = PeakSignalNoiseRatio(device=device)
     for pred, target in zip(preds, targets):
@@ -11,17 +10,30 @@ def calculatePSNR(preds, targets, device):
 def calcSSIM(preds, targets, device):
     vals = []
     for pred, target in zip(preds, targets):
-        pred = pred.squeeze()
-        target = target.squeeze()
+        # pred = pred.squeeze()
+        # target = target.squeeze()
         pred = pred.detach().to('cpu').numpy()
         target = target.detach().to('cpu').numpy()
-        print(pred.shape, target.shape)
         vals.append(ssim(pred, target, channel_axis=0, gaussian_weights=True, data_range=(target.max()-target.min())))
     mean_ssim = sum(vals) / len(vals)
     return mean_ssim
     
-
-
+class SSIM:
+    def __init__(self):
+        self.vals = []
+        
+    def update(self, pred, target):
+        if len(pred.shape) > 3 and pred.shape[0] > 1: 
+            for pred, target in zip(pred, target):
+                self.vals.append(calcSSIM(pred, target, 'cpu'))
+                
+        else:
+            pred = pred.squeeze() 
+            self.vals.append(calcSSIM(pred, target, 'cpu'))
+    def compute(self):
+        if len(self.vals) == 0:
+            "Metric not computed. No images are compared. Use object.update()"
+        else: return sum(self.vals) / len(self.vals)
 
 
         
